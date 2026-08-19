03b3af66a5ea35bab830a8bd41cc489fcf22a5fc
DainnelChen <143495110+DainnelChen@users.noreply.github.com>
2026-08-19 14:26:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,10 +1,11 @@
 /* eslint-disable */
 
 /**
- * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 此项目配置为方便新人使用，已缩减至精简配置。
+ * 如若想使用更多功能，请查看文档中的【3. config参数说明】
  * 自行添加属性，以支持更多个性化功能
  */
+
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
@@ -13,25 +14,25 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '四川',
+  CITY: '成都',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o6gdu2_BYmTiM3FUAYwflZuoJbM4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'a17la1QGJrPuCxh_dCCv-0j-of84o3TPwtqmakSJ-Ts',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        // 注意：此条配置日期为阴历日期，因为`type`中 "生日" 之前有 * 符号
         {
           type: '*生日', name: '宝贝', year: '1996', date: '09-09',
         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        // 注意：此条配置日期为阳历日期，因为`type`中 "生日" 之前没有 * 符号
         {
           type: '生日', name: '李四', year: '1996', date: '09-31',
         },
@@ -49,7 +50,6 @@ const USER_CONFIG = {
     },
   ],
 
-
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: '',
 
@@ -64,4 +64,3 @@ const USER_CONFIG = {
 }
 
 module.exports = USER_CONFIG
-
